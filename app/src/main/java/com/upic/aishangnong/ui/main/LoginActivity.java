ea58afad4c6b83f612f39bccd7436612ebeedb80
ZF <986299685@qq.com>
2016-11-30 19:04:06 +0800

    zf

---
+++ app/src/main/java/com/upic/aishangnong/ui/main/LoginActivity.java
@@ -0,0 +1,32 @@
+package com.upic.aishangnong.ui.main;
+
+import android.widget.Button;
+
+import com.upic.aishangnong.R;
+import com.upic.aishangnong.ui.base.BaseActivity;
+
+/**
+ * Created by ZF on 2016/11/30.
+ */
+
+public class LoginActivity extends BaseActivity{
+    @Override
+    public int getContentView() {
+        return R.layout.activity_login;
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initClick() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+}
